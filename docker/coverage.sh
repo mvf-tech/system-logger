65eb637d0ff9955638937a5b95fd870f15a11d36
Dovydas Rupsys <dovydas.rupsys@cryptohaven.com>
2018-10-03 21:17:10 +0100

    Use docker image instead of local dockerfile

--- docker/coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-vendor/bin/php-cs-fixer fix
-vendor/bin/phpcbf --standard=mvf_ruleset.xml
-vendor/bin/phpcs --standard=mvf_ruleset.xml
-docker-php-ext-enable xdebug
-COMMAND="vendor/bin/phpspec run -f pretty -v"
-shape exec --command "$COMMAND" --shape phpspec.yml extensions=phpspec-coverage.yml
-rm /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
